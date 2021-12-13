2921206c37b8cb012e92b0cdff6d056b931ef72c
Quentin <quentin.andre@imerir.com>
2021-12-13 18:47:16 +0100

    MQTT fonctionnel

--- robot/esp/LQTT/src/main.cpp
+++ robot/esp/LQTT/src/main.cpp
@@ -1,28 +1,30 @@
 #include <Arduino.h>
-#include <PubSubClient.h>
 
 #include "motor.h"
 #include "uart.h"
+#include "mqtt.h"
 
 void setup() {
   
   uart::init();
-
   motor::init();
-  //motor::set();
+  mqtt::init();
 
 }
 
 void loop() {
   
-  uint8_t in = uart::read();
+  /*uint8_t in = uart::read();
 
   if(in == uart::STOP) {
     motor::reset();
   }
   else if(in == uart::RESUME) {
     motor::set();
-  }
+  }*/
 
-  delay(100);
+  mqtt::test();
+
+  delay(1000);
+
 }
